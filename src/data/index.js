36ae0f4844f7c497678b8a44e8f61be25d755c94
mr-scrpt <mr.scrpt@gmail.com>
2022-06-30 18:51:54 +0300

    implemented: game list section

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,8 @@ import * as nodePath from "path";
 const srcFolder = nodePath.basename(`${nodePath.resolve()}/src`);
 
 export const data = {
-	menu: JSON.parse(readFileSync(`${srcFolder}/data/menu.json`, "utf8")),
-	phone: JSON.parse(readFileSync(`${srcFolder}/data/phone.json`, "utf8")),
-	social: JSON.parse(readFileSync(`${srcFolder}/data/social.json`, "utf8")),
+  menu: JSON.parse(readFileSync(`${srcFolder}/data/menu.json`, "utf8")),
+  phone: JSON.parse(readFileSync(`${srcFolder}/data/phone.json`, "utf8")),
+  social: JSON.parse(readFileSync(`${srcFolder}/data/social.json`, "utf8")),
+  games: JSON.parse(readFileSync(`${srcFolder}/data/games.json`, "utf8")),
 };
